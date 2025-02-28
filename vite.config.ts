2fb79e47a562e333bd820a615deb1b9a39332ad1
henriandrade <henrique.andrade@unesp.br>
2025-02-28 15:20:39 -0300

    cvjs2

--- vite.config.ts
+++ vite.config.ts
@@ -22,23 +22,9 @@ export default defineConfig(({ command }) => {
         entry: path.resolve(__dirname, 'src/main.ts'),
         name: 'FloragComponents',
         fileName: 'florag-webflow-animation',
-        formats: ['es', 'umd'], // Add ES module format
+        formats: ['umd'], // Specify UMD format for browser compatibility
       } : undefined,
-      rollupOptions: {
-        // Make sure to externalize deps that shouldn't be bundled
-        // into your library
-        external: ['svelte'],
-        output: {
-          // Provide globals for external packages
-          globals: {
-            svelte: 'Svelte',
-          },
-          // Set correct MIME type for UMD bundle
-          format: 'umd'
-        },
-      },
     },
-    assetsInclude: ['src/lib/webflow/index.html'],
     resolve: {
       alias: {
         '@': path.resolve('./src'),
